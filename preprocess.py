633c92437415f81b2aa7ebb3ae535ebbbdc4fd99
Yichung Hsu <fov223@gmail.com>
2018-03-22 07:39:29 +0800

    printout result and reformat code

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 def yolo_box(gt_box, image_width, image_height, cell_length):
-
     num_class = max([i[0] for i in gt_box]) + 1
     cell_width, cell_height = image_width // cell_length, image_height // cell_length
 
@@ -14,6 +13,6 @@ def yolo_box(gt_box, image_width, image_height, cell_length):
         x_cell, y_cell = box_middle_point_x // cell_length, box_middle_point_y // cell_length
 
         target[x_cell, y_cell, :5] = np.array([1, l, t, r, b])
-        target[x_cell, y_cell, 5+class_num] = 1
+        target[x_cell, y_cell, 5 + class_num] = 1
 
-    return target.reshape(cell_width*cell_height, -1)
+    return target.reshape(cell_width * cell_height, -1)
